adfc4b778b1ed8b558a49daef1cedc58d99ec81a
CORP\asarkar <asarkar@qualys.com>
2020-01-18 23:24:00 +0530

    Updates

---
+++ precache-manifest.a994982a28ffd47018dabed82173af97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c499fa71ac11bc6edfde7a442b7b272",
+    "url": "/SampleDemoApp/index.html"
+  },
+  {
+    "revision": "b02d98de27c284ae83d7",
+    "url": "/SampleDemoApp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e32322022d5acd7c7503",
+    "url": "/SampleDemoApp/static/js/2.db617fd4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/SampleDemoApp/static/js/2.db617fd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b02d98de27c284ae83d7",
+    "url": "/SampleDemoApp/static/js/main.9978971e.chunk.js"
+  },
+  {
+    "revision": "76272d23445f7bcc6c51",
+    "url": "/SampleDemoApp/static/js/runtime-main.6b0334cc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SampleDemoApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
